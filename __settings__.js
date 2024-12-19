72f9af2352673604b8ce9840f4a0048cc6949963
MM-ivre <mikhail@immersivevreducation.com>
2024-12-19 19:45:57 +0200

    ed

---
+++ __settings__.js
@@ -0,0 +1,21 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "2107195.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'deviceTypes': [`webgl2`, `webgl1`],
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 202851764, 202851763, 202851765, 202851766, 202890278, 202890280, 202890317, 202890318, 202900152, 202900153, 202900154, 202900155, 202900156, 202900157, 202912255, 203139945 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+];
